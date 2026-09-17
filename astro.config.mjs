dc0ad05374c6e665c41de1de79a5a8ababb4805c
gjikamelisa-design <gjikamelisa@gmail.com>
2026-09-17 12:34:16 +0200

    Use no-slash canonical URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://fitnessxmarketing.com',
+  trailingSlash: 'never',
   build: {
     inlineStylesheets: 'always',
   },
